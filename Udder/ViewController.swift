d71396a78c2876451b459b893c54cbbf9a1320bc
Jun Itokawa <jitokawa96@gmail.com>
2020-09-05 09:35:23 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jun Itokawa on 2020/09/05.
+//  Copyright © 2020 Jun Itokawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
